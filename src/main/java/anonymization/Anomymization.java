e30d7a6fcf306017b29482806f48138a6e92b7b5
Duc Thai Vu <thaivd1309@gmail.com>
2022-05-29 11:35:35 +0200

    a

---
+++ src/main/java/anonymization/Anomymization.java
@@ -0,0 +1,12 @@
+package anonymization;
+
+import org.apache.flink.table.api.Table;
+
+public class Anomymization {
+    Table table;
+    MaskingFunctions maskingFunctions = new MaskingFunctions();
+
+    public void generalization() {
+
+    }
+}
--- src/main/java/anonymization/MaskingFunctions.java
+++ src/main/java/anonymization/MaskingFunctions.java
@@ -1,20 +1,23 @@
 package anonymization;
 
+import org.apache.flink.table.api.Table;
+
 public class MaskingFunctions {
 
     public String supress(){
         return "";
     }
-    public String generalize() {
+    public String generalize(Table column, Tree tree) {
         return "";
     }
+    //generalization(List list, String value)
     public String tokenize() {
         return "";
     }
     public String bucketize() {
         return "";
     }
-    public double addNoise() {
+    public double addNoise(Table column) {
         return 0;
     }
 }
